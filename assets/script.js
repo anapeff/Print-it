4b72b02c734a232d3815b44a41b47efa861c8e05
anapeff <anais.pefferkorn@outlook.fr>
2023-12-30 12:34:23 +0100

    Ajout event arrow

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,32 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+const arrowLeft = document.querySelector(".arrow_left")
+const arrowRight = document.querySelector(".arrow_right")
+const imageBanner = document.querySelector(".banner-img")
+const texte = document.querySelector(".banner-txt");
+
+let i = 0;
+// flèche gauche 
+arrowLeft.addEventListener("click", () => {
+	i--;
+	if (i < 0) {
+		i = slides.length - 1;
+	}
+
+// Mise à jour de l'image et du texte
+imageBanner.src = "assets/images/slideshow/" + slides[i].image;
+texte.innerHTML = slides[i].tagLine;
+});
+
+// flèche droite
+arrowRight.addEventListener("click", () => {
+	i++;
+	if (i > slides.length) {
+		i=0;
+	}
+
+// Mise à jour de l'image et du texte
+imageBanner.src = "assets/images/slideshow/" + slides[i].image;
+texte.innerHTML = slides[i].tagLine;
+});
